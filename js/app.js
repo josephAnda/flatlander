c1c08218da0b76353b65789bfbd28bb7bda8da71
josephAnda <orenmurasaki@gmail.com>
2017-02-11 14:02:43 -0800

    added filters, repeaters, and ng-src

--- js/app.js
+++ js/app.js
@@ -2,14 +2,37 @@
 	var app = angular.module('store', [ ]);
 
 	app.controller('StoreController', function() {
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: ' . . . . '
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: 'Some gems have hidden qualities beyond their luster, \
+			beyond their shine...Dodeca is one of those gems.',
+			images: [
+			  "http://placehold.it/200x100",
+			  "http://placehold.it/100x99", 
+			  "http://placehold.it/100x98"
+			],
+			canPurchase: true,
+			soldOut: false,
 
-	}
+		},
+		{
+			name: "Pentagonal Gem",
+			price: 5.95,
+			description: 'This wondrous piece of metal is the pride and joy of \
+			the Valkyria Clan.',
+			images: [
+			  "http://placehold.it/200x100",
+			  "http://placehold.it/100x99", 
+			  "http://placehold.it/100x98"
+			],
+			canPurchase: true,
+			soldOut: false,
+		}
+	];
 
 })();
