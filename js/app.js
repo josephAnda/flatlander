a4026fc2bec2a9af0cd25d64a04ea071c6223b1d
josephAnda <orenmurasaki@gmail.com>
2017-02-11 15:57:21 -0800

    added two way bindings and new attributes for items

--- js/app.js
+++ js/app.js
@@ -4,13 +4,43 @@
 	app.controller('StoreController', function() {
 		this.products = gems;
 	});
+	app.controller('PanelController', function() {
+		this.tab = 1;
 
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		}
+
+	});
 	var gems = [
 		{
+			name: 'Azurite',
+			description: 'This gem has a mysterious blue glow that comes from a \
+			steady surge of energy',
+			shine: 9,
+			price: 22.90,
+			rarity: 6,
+			color: '#EEE',
+			faces: 12,
+			images: [
+			  
+			],
+			canPurchase: true,
+			soldOut: false,
+		},
+		{
 			name: 'Dodecahedron',
-			price: 2.95,
+			
 			description: 'Some gems have hidden qualities beyond their luster, \
 			beyond their shine...Dodeca is one of those gems.',
+			shine: 70,
+			price: 2.95,
+			rarity: 2,
+			color: '#000',
+			faces: 6,
 			images: [
 			  "http://placehold.it/200x100",
 			  "http://placehold.it/100x99", 
@@ -22,9 +52,13 @@
 		},
 		{
 			name: "Pentagonal Gem",
-			price: 5.95,
 			description: 'This wondrous piece of metal is the pride and joy of \
 			the Valkyria Clan.',
+			shine: 8,
+    		price: 110.50,
+    		rarity: 7,
+    		color: '#CCC',
+    		faces: 14,
 			images: [
 			  "http://placehold.it/200x100",
 			  "http://placehold.it/100x99", 
